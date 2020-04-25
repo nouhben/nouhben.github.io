0952c0c23888a71d449d507e1d9a9abfd0d255e6
nouh ben kadi <benkadi.nouh@icloud.com>
2020-04-25 18:47:09 +0100

    added the font poppins as font family

--- js/main.js
+++ js/main.js
@@ -1,39 +1,3 @@
-/*
-//Create a function to select elements
-
-const selectElement = (element) => document.querySelector(element);
-
-//call the select element function to get values into variables
-
-const header = selectElement('header');
-const main = selectElement('main');
-//add clikc events to trigger the active state
-selectElement('.menu').addEventListener('click', () => {
-    header.classList.toggle('active');
-    selectElement('main').classList.toggle('active');
-});
-
-//when clicking outside the nav close it
-window.onclick = (event) => {
-    if (event.target.matches('.active')) {
-        console.log(event.target);
-        if (header.classList.contains('active')) {
-            header.classList.remove('active');
-
-        }
-    }
-};
-//
-
-function closeMenu() {
-    const header = selectElement('header'); if (header.classList.contains('active')) {
-        header.classList.remove('active');
-
-    };
-}
-
-
-*/
 //var isActive = false;
 const selectElement = (element) => document.querySelector(element);
 const header = selectElement('header');
